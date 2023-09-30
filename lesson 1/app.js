6f895ab50dc8d001aa222a20940d4b8f93e4aeb8
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-30 11:56:31 +0630

    Update app.js

--- lesson 1/app.js
+++ lesson 1/app.js
@@ -1,6 +1,3 @@
-var url = "https://mylogger.io/log"
-
-function log(message){
-    console.log(message)
-}
-module.exports.log = log;
+var logger = require("./logger")
+logger.log("hello")
+console.log(logger.url)
